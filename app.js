268e42b6fa1ef27e734f7dd82c75d1dd069923b6
abshir112 <abshir_muhumed.abdi0018@stud.hkr.se>
2024-04-06 22:48:30 +0200

    created the endpoints

--- app.js
+++ app.js
@@ -24,7 +24,78 @@ mongoose.connect(process.env.MONGODB_URI, {})
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+    });
 
+//show all recipes
+app.get('/api/recipes', async (req, res) => {
+    try {
+        const recipes = await getAllRecipes();
+        res.json(recipes);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+//show a single recipe by title
+app.get('/api/recipes/:title', async (req, res) => {
+    try {
+        const recipe = await getRecipeByTitle(req.params.title);
+        if (recipe) {
+            res.json(recipe);
+        } else {
+            res.status(404).json({ message: 'Recipe not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+//add a new recipe
+app.post('/api/recipes', async (req, res) => {
+    try {
+        const { title, description, ingredients } = req.body;
+        const recipe = await createRecipe(title, description, ingredients);
+        res.status(201).json(recipe);
+    } catch (error) {
+        if (error.code === 11000) { // MongoDB duplicate key error
+            res.status(409).json({ message: 'Recipe already exists' });
+        } else {
+            res.status(400).json({ message: error.message });
+        }
+    }
+});
+
+//update a recipe
+app.put('/api/recipes/:id', async (req, res) => {
+    try {
+        const updatedRecipe = await updateRecipeById(req.params.id, req.body);
+        if (updatedRecipe) {
+            res.json(updatedRecipe);
+        } else {
+            res.status(404).json({ message: 'Recipe doesn\'t exist' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+//delete a recipe
+app.delete('/api/recipes/:id', async (req, res) => {
+    try {
+        const recipe = await deleteRecipeById(req.params.id);
+        if (recipe) {
+            res.json({ message: 'Recipe deleted' });
+        } else {
+            res.status(404).json({ message: 'Recipe doesn\'t exist' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 
 
